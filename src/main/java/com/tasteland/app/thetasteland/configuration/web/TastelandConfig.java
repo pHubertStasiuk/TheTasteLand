9fdcd10e0bec2936012f260fe78f761a1978d45b
hstasiuk <p.hubert.stasiuk@gmail.com>
2019-06-01 15:11:16 +0200

    Setting up a JPA configuration to Database

---
+++ src/main/java/com/tasteland/app/thetasteland/configuration/web/TastelandConfig.java
@@ -0,0 +1,81 @@
+package com.tasteland.app.thetasteland.configuration.web;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.dao.annotation.PersistenceExceptionTranslationPostProcessor;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.persistence.EntityManagerFactory;
+import javax.sql.DataSource;
+import java.util.Properties;
+
+@Configuration
+@EnableTransactionManagement
+@ComponentScan(basePackages = "com.tasteland.app")
+@PropertySource("classpath:persistence-mysql.properties")
+public class TastelandConfig {
+
+    private final Environment environment;
+
+    @Autowired
+    public TastelandConfig(Environment environment) {
+        this.environment = environment;
+    }
+
+    @Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName(getProp("jdbc.driver"));
+        dataSource.setUrl(getProp("jdbc.url"));
+        dataSource.setUsername(getProp("jdbc.user"));
+        dataSource.setPassword(getProp("jdbc.password"));
+        return dataSource;
+
+    }
+    private String getProp(String property) {
+        return environment.getProperty(property);
+    }
+
+    private Properties setJpaProperties() {
+        Properties props = new Properties();
+        props.setProperty("hibernate.dialect", getProp("hibernate.dialect"));
+        props.setProperty("hibernate.show_sql", getProp("hibernate.show_sql"));
+        props.setProperty("hibernate.format_sql", getProp("hibernate.format_sql"));
+        return props;
+    }
+
+    @Bean
+    public LocalContainerEntityManagerFactoryBean entityManagerFactoryBean() {
+        JpaVendorAdapter jpaVendorAdapter = new HibernateJpaVendorAdapter();
+        LocalContainerEntityManagerFactoryBean entityManagerFactoryBean = new LocalContainerEntityManagerFactoryBean();
+        entityManagerFactoryBean.setDataSource(dataSource());
+        entityManagerFactoryBean.setPackagesToScan(getProp("hibernate.packagesToScan"));
+        entityManagerFactoryBean.setJpaVendorAdapter(jpaVendorAdapter);
+        entityManagerFactoryBean.setJpaProperties(setJpaProperties());
+        return entityManagerFactoryBean;
+    }
+
+    @Bean
+    @Autowired
+    public PlatformTransactionManager transactionManager(EntityManagerFactory emf){
+        JpaTransactionManager jpaTransactionManager = new JpaTransactionManager();
+        jpaTransactionManager.setEntityManagerFactory(emf);
+        return jpaTransactionManager;
+    }
+
+    @Bean
+    public PersistenceExceptionTranslationPostProcessor exceptionTranslation(){
+        return new PersistenceExceptionTranslationPostProcessor();
+    }
+}
+
