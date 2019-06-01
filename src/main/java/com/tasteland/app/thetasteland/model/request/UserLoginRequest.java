271a143fc4cc0d2448c5ca79fc763440cb65702b
hstasiuk <p.hubert.stasiuk@gmail.com>
2019-06-01 18:38:20 +0200

    Simple UserLoginRequest model impl

---
+++ src/main/java/com/tasteland/app/thetasteland/model/request/UserLoginRequest.java
@@ -0,0 +1,16 @@
+package com.tasteland.app.thetasteland.model.request;
+
+import lombok.Data;
+
+@Data
+public class UserLoginRequest {
+
+    private final String email;
+    private final String password;
+
+    public UserLoginRequest(String email, String password) {
+        this.email = email;
+        this.password = password;
+    }
+}
+
