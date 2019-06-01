efd378234d23415426a77a0a396a3a216664788e
hstasiuk <p.hubert.stasiuk@gmail.com>
2019-06-01 13:57:34 +0200

    A setup for the new The Tasteland project

---
+++ src/main/java/com/tasteland/app/thetasteland/ThetastelandApplication.java
@@ -0,0 +1,13 @@
+package com.tasteland.app.thetasteland;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThetastelandApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThetastelandApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tasteland/app/thetasteland/ThetastelandApplicationTests.java
@@ -0,0 +1,16 @@
+package com.tasteland.app.thetasteland;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ThetastelandApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
