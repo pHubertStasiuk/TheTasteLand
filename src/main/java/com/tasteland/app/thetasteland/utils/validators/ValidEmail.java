ed8c94627742c3e65014054b3743329f65f94287
hstasiuk <p.hubert.stasiuk@gmail.com>
2019-06-01 17:37:58 +0200

    Email Validators amendments

--- src/main/java/com/tasteland/app/thetasteland/utils/validators/ValidEmail.java
+++ src/main/java/com/tasteland/app/thetasteland/utils/validators/ValidEmail.java
@@ -9,9 +9,7 @@ import java.lang.annotation.*;
 @Constraint(validatedBy = EmailValidator.class)
 @Target({ElementType.FIELD, ElementType.TYPE, ElementType.ANNOTATION_TYPE})
 public @interface ValidEmail {
-
     String message() default "Invalid email address";
     Class<?>[] groups() default {};
     Class<? extends Payload>[] payload() default {};
-
 }
