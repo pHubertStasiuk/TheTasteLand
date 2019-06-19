f2218930f10f7a78a2f9bdf87799d3accd7f8d9d
hstasiuk <p.hubert.stasiuk@gmail.com>
2019-06-19 21:18:33 +0200

    Simple Role Entity class

---
+++ src/main/java/com/tasteland/app/thetasteland/model/auth/Role.java
@@ -0,0 +1,26 @@
+package com.tasteland.app.thetasteland.model.auth;
+
+import lombok.Data;
+import org.springframework.security.core.GrantedAuthority;
+
+import javax.persistence.*;
+
+
+@Entity
+@Table(name = "role")
+@Data
+public class Role implements GrantedAuthority {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+    @Column(name = "name")
+    private String name;
+
+
+    @Override
+    public String getAuthority() {
+        return getName();
+    }
+}
