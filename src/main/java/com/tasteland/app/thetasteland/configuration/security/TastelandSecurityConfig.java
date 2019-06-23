7d96acde6b3a6a0d96a5876659d0f29feb44d7b9
hstasiuk <p.hubert.stasiuk@gmail.com>
2019-06-23 16:59:14 +0200

    TastelandSecurityConfig adjustments - configure method impl and PropertyUtils added

--- src/main/java/com/tasteland/app/thetasteland/configuration/security/TastelandSecurityConfig.java
+++ src/main/java/com/tasteland/app/thetasteland/configuration/security/TastelandSecurityConfig.java
@@ -1,67 +1,77 @@
 package com.tasteland.app.thetasteland.configuration.security;
 
 import com.tasteland.app.thetasteland.service.AuthenticationService;
+import com.tasteland.app.thetasteland.utils.PropertyUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
-import org.springframework.core.env.Environment;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-import java.util.Optional;
 
 @Configuration
 @EnableWebSecurity
 @PropertySource("classpath:security-auth.properties")
 public class TastelandSecurityConfig extends WebSecurityConfigurerAdapter {
 
-    private final Environment environment;
+    private final PropertyUtils prop;
     private final AuthenticationService authenticationService;
+    private final AuthenticationEntryPoint entryPoint;
+    private final AuthenticationFilter filter;
 
     @Autowired
-    public TastelandSecurityConfig(Environment environment, AuthenticationService authenticationService) {
-        this.environment = environment;
+    public TastelandSecurityConfig(PropertyUtils prop, AuthenticationService authenticationService,
+                                   AuthenticationEntryPoint entryPoint, AuthenticationFilter filter) {
+        this.prop = prop;
         this.authenticationService = authenticationService;
+        this.entryPoint = entryPoint;
+        this.filter = filter;
     }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
-        Optional<Integer> str = getIntProp("bcrypt.strength");
-        return str.isPresent() ?
-                new BCryptPasswordEncoder(str.get()) :
-                new BCryptPasswordEncoder();
-
+        Integer strength = prop.getInt("bcrypt.strength");
+        return new BCryptPasswordEncoder(strength);
     }
 
-    private Optional<Integer> getIntProp(String property) {
-        try {
-            String prop = environment.getProperty(property);
-            return Optional.of(Integer.parseInt(prop));
-        } catch (NumberFormatException nfe) {
-            return Optional.empty();
-        }
+    @Override
+    @Bean
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
     }
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        super.configure(auth);
+        auth.userDetailsService(authenticationService).passwordEncoder(passwordEncoder());
     }
 
     @Override
     public void configure(WebSecurity web) throws Exception {
-        super.configure(web);
+        web.ignoring().antMatchers(HttpMethod.POST, "/auth");
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        super.configure(http);
+        http
+
+                .csrf().disable()
+                .exceptionHandling().authenticationEntryPoint(entryPoint).and()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
+                .authorizeRequests()
+                .antMatchers("/auth/**").permitAll()
+                .antMatchers("/api/**").authenticated()
+                .anyRequest().authenticated();
+        http
+                .addFilterBefore(filter, UsernamePasswordAuthenticationFilter.class);
     }
-
-
 }
