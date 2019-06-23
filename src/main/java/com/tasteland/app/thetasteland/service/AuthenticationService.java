066d48936f8671df43a234e1f5f3e847080e3c20
hstasiuk <p.hubert.stasiuk@gmail.com>
2019-06-23 16:57:38 +0200

    AuthenticationService interface - logout method  added

--- src/main/java/com/tasteland/app/thetasteland/service/AuthenticationService.java
+++ src/main/java/com/tasteland/app/thetasteland/service/AuthenticationService.java
@@ -1,6 +1,13 @@
 package com.tasteland.app.thetasteland.service;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.userdetails.UserDetailsService;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 public interface AuthenticationService extends UserDetailsService {
+
+    ResponseEntity<?> logout(HttpServletRequest request, HttpServletResponse response);
+
 }
