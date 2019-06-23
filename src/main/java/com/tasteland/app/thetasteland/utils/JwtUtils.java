53a7b22a0f4e6121d250e118a58faf945d9231a4
hstasiuk <p.hubert.stasiuk@gmail.com>
2019-06-23 16:53:47 +0200

    JwtUtils impl -   isValidToken method and additional claims added in generateToken

--- src/main/java/com/tasteland/app/thetasteland/utils/JwtUtils.java
+++ src/main/java/com/tasteland/app/thetasteland/utils/JwtUtils.java
@@ -1,34 +1,40 @@
 package com.tasteland.app.thetasteland.utils;
 
-import com.tasteland.app.thetasteland.entity.UserEntity;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Clock;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.impl.DefaultClock;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.Data;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.core.env.Environment;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
 
-import javax.xml.crypto.Data;
 import java.io.Serializable;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.function.Function;
 
-
+@Component
+@Data
 public class JwtUtils implements Serializable {
 
     private static final long serialVersionUID = 7326262609992241652L;
     private final Clock clock = DefaultClock.INSTANCE;
     private final Long expiration;
     private final String secret;
-
+    private final String tokenHeader;
+    private final String jwtPrefix;
 
     public JwtUtils(
             @Value("${jwt.expiration}") Long expiration,
-            @Value("${jwt.secret}") String secret) {
+            @Value("${jwt.secret}") String secret,
+            @Value("${jwt.header}") String tokenHeader,
+            @Value("${jwt.prefix}") String jwtPrefix
+    ) {
+        this.tokenHeader = tokenHeader;
+        this.jwtPrefix = jwtPrefix;
         this.expiration = expiration;
         this.secret = secret;
     }
@@ -60,18 +66,22 @@ public class JwtUtils implements Serializable {
         return expirationDate.before(clock.now());
     }
 
-    public String generateToken(UserEntity user) {
+    public String generateToken(UserDetails user) {
         Map<String, Object> claims = new HashMap<>();
+        claims.put("roles", user.getAuthorities());
+        claims.put("enabled", user.isEnabled());
+        claims.put("expired", user.isAccountNonExpired());
+        claims.put("locked", user.isAccountNonLocked());
         return createToken(claims, user);
     }
 
-    private String createToken(Map<String, Object> claims, UserEntity user) {
+    private String createToken(Map<String, Object> claims, UserDetails user) {
         final Date createdDate = clock.now();
         final Date expirationDate = calculateExpirationDate(createdDate);
         return Jwts.builder()
                 .setClaims(claims)
                 .setExpiration(expirationDate)
-                .setSubject(user.getEmail())
+                .setSubject(user.getUsername())
                 .setIssuedAt(createdDate)
                 .signWith(SignatureAlgorithm.HS512, secret)
                 .compact();
@@ -81,4 +91,16 @@ public class JwtUtils implements Serializable {
         return new Date(createdDate.getTime() + expiration * 1000);
     }
 
+    public boolean containsToken(String tokenHeader) {
+        return tokenHeader != null && tokenHeader.startsWith(getJwtPrefix());
+    }
+
+    public Boolean isValidToken(String token, UserDetails userDetails) {
+        final String username = getUsernameFromToken(token);
+        final Date expiration = getExpirationDateFromToken(token);
+        return (
+                username.equals(userDetails.getUsername())
+                        && !isExpired(token)
+                        && expiration.after(clock.now()));
+    }
 }
