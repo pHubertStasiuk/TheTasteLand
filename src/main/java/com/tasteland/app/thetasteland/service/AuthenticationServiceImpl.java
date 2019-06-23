1ff440edba1d9ad4947cbb6bafda4b4d14684e58
hstasiuk <p.hubert.stasiuk@gmail.com>
2019-06-23 16:51:08 +0200

    User logout functionality implementation

--- src/main/java/com/tasteland/app/thetasteland/service/AuthenticationServiceImpl.java
+++ src/main/java/com/tasteland/app/thetasteland/service/AuthenticationServiceImpl.java
@@ -1,17 +1,28 @@
 package com.tasteland.app.thetasteland.service;
 
 import com.tasteland.app.thetasteland.entity.UserEntity;
+import com.tasteland.app.thetasteland.model.web.response.HttpResponse;
 import com.tasteland.app.thetasteland.repository.UserRepository;
+import com.tasteland.app.thetasteland.utils.HttpMessage;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import java.util.ArrayList;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Objects;
 
 @Service
-public class AuthenticationServiceImpl implements AuthenticationService{
+@Transactional
+public class AuthenticationServiceImpl implements AuthenticationService {
 
     private final UserRepository userRepository;
 
@@ -20,12 +31,24 @@ public class AuthenticationServiceImpl implements AuthenticationService{
         this.userRepository = userRepository;
     }
 
+
+    @Override
+    public ResponseEntity<?> logout(HttpServletRequest request, HttpServletResponse response) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (Objects.nonNull(authentication)) {
+            new SecurityContextLogoutHandler().logout(request, response, authentication);
+        } else {
+            return new ResponseEntity<>(new HttpResponse(HttpMessage.USER_LOGOUT_FAILED), HttpStatus.CONFLICT);
+        }
+        return new ResponseEntity<>(new HttpResponse(HttpMessage.USER_LOGOUT_SUCCESSFUL), HttpStatus.OK);
+    }
+
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
         UserEntity userEntity = userRepository.findByEmail(email);
-        if(userEntity == null) {
+        if (userEntity == null) {
             throw new UsernameNotFoundException("Invalid user credentials");
         }
-        return new User(userEntity.getEmail(), userEntity.getEncryptedPassword(), new ArrayList<>());
+        return new User(userEntity.getEmail(), userEntity.getEncryptedPassword(), userEntity.getRoles());
     }
 }
