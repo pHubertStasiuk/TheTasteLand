9049e1d745634d4543b1b75e42549964168e3f27
hstasiuk <p.hubert.stasiuk@gmail.com>
2019-06-23 17:00:18 +0200

    UserService interface and implementation

--- src/main/java/com/tasteland/app/thetasteland/service/UserService.java
+++ src/main/java/com/tasteland/app/thetasteland/service/UserService.java
@@ -1,7 +1,16 @@
 package com.tasteland.app.thetasteland.service;
 
-import com.tasteland.app.thetasteland.shared.UserDTO;
+import com.tasteland.app.thetasteland.shared.dto.UserDTO;
+
+import java.util.Optional;
 
 public interface UserService {
-    UserDTO createUser(UserDTO userDTO);
+    UserDTO save(UserDTO userDTO);
+    Optional<UserDTO> getByEmail(String email);
+    Optional<UserDTO> getByUserId(String userId);
+    void deleteById(String userId);
+
+
+
+
 }
--- src/main/java/com/tasteland/app/thetasteland/service/UserServiceImpl.java
+++ src/main/java/com/tasteland/app/thetasteland/service/UserServiceImpl.java
@@ -1,41 +1,67 @@
 package com.tasteland.app.thetasteland.service;
 
 import com.tasteland.app.thetasteland.entity.UserEntity;
+import com.tasteland.app.thetasteland.repository.RoleRepository;
 import com.tasteland.app.thetasteland.repository.UserRepository;
-import com.tasteland.app.thetasteland.shared.UserDTO;
-import com.tasteland.app.thetasteland.shared.UserAlreadyExistsException;
+import com.tasteland.app.thetasteland.shared.dto.UserDTO;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDate;
+import java.util.Collections;
+import java.util.Optional;
+
 @Service
+@Transactional
 public class UserServiceImpl implements UserService {
 
     private final UserRepository userRepository;
+    private final RoleRepository roleRepository;
 
     @Autowired
-    public UserServiceImpl(UserRepository userRepository) {
+    public UserServiceImpl(UserRepository userRepository, RoleRepository roleRepository) {
         this.userRepository = userRepository;
+        this.roleRepository = roleRepository;
     }
 
     @Override
-    @Transactional
-    public UserDTO createUser(UserDTO userDTO) {
-        if (userRepository.findByEmail(userDTO.getEmail()) != null)
-            throw new UserAlreadyExistsException("User already exists!");
-        UserEntity userEntity = UserEntity.builder()
-                .userId(userDTO.getUserId())
-                .email(userDTO.getEmail())
-                .firstName(userDTO.getFirstName())
-                .lastName(userDTO.getLastName())
-                .emailVerificationToken(userDTO.getEmailVerificationToken())
-                .encryptedPassword(userDTO.getEncryptedPassword())
-                .emailVerified(userDTO.getEmailVerified())
-                .build();
-        UserEntity storedUser = userRepository.save(userEntity);
-        UserDTO user = new UserDTO();
-        BeanUtils.copyProperties(storedUser, user);
-        return user;
+    public UserDTO save(UserDTO userDTO) {
+        UserEntity userEntity = new UserEntity();
+        BeanUtils.copyProperties(userDTO, userEntity);
+        userEntity.setAccountCreationDate(LocalDate.now());
+        userEntity.setLastPasswordReset(LocalDate.now());
+        userEntity.setRoles(Collections.singleton(roleRepository.findRoleByName("ROLE_USER")));
+        userRepository.save(userEntity);
+        BeanUtils.copyProperties(userEntity, userDTO);
+        return userDTO;
+    }
+
+    @Override
+    public Optional<UserDTO> getByEmail(String email) {
+        UserEntity user = userRepository.findByEmail(email);
+        UserDTO userDTO = new UserDTO();
+        if (user != null) {
+            BeanUtils.copyProperties(user, userDTO);
+            return Optional.of(userDTO);
+        }
+        return Optional.empty();
+    }
+
+    @Override
+    public Optional<UserDTO> getByUserId(String userId) {
+
+        UserEntity userFromDB = userRepository.findByUserId(userId);
+        Optional<UserEntity> user = Optional.of(userFromDB);
+        Optional<UserDTO> userDTO = Optional.of(new UserDTO());
+        BeanUtils.copyProperties(user, userDTO);
+        return userDTO;
+    }
+
+    @Override
+    public void deleteById(String userId) {
+        UserEntity userEntity;
+        if ((userEntity = userRepository.findByUserId(userId)) != null) userRepository.delete(userEntity);
     }
 }
