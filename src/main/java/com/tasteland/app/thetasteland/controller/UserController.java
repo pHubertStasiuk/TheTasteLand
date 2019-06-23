899a4771609042f2b2607c9bd1c14838c8cf2191
hstasiuk <p.hubert.stasiuk@gmail.com>
2019-06-23 17:03:14 +0200

    UserController - InitBinder && create endpoint impl for registration purposes

--- src/main/java/com/tasteland/app/thetasteland/controller/UserController.java
+++ src/main/java/com/tasteland/app/thetasteland/controller/UserController.java
@@ -1,18 +1,67 @@
 package com.tasteland.app.thetasteland.controller;
 
 
+import com.tasteland.app.thetasteland.model.web.request.UserRegisterRequest;
+import com.tasteland.app.thetasteland.model.web.response.HttpResponse;
+import com.tasteland.app.thetasteland.model.web.response.UserDetailsResponse;
+import com.tasteland.app.thetasteland.service.UserService;
+import com.tasteland.app.thetasteland.shared.dto.UserDTO;
+import com.tasteland.app.thetasteland.utils.HttpMessage;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.Optional;
 
 @RestController
-@RequestMapping(path = "/users")
+@RequestMapping(path = "/api/users")
 public class UserController {
 
+    private final UserService userService;
+    private final PasswordEncoder encoder;
 
-    @GetMapping
-    public String getUser() {
-        return "User";
+    @Autowired
+    public UserController(UserService userService, PasswordEncoder encoder) {
+        this.userService = userService;
+        this.encoder = encoder;
+    }
+
+
+    @InitBinder
+    public void initUserRequest(WebDataBinder webDataBinder) {
+        StringTrimmerEditor stringTrimmerEditor = new StringTrimmerEditor(true);
+        webDataBinder.registerCustomEditor(String.class, stringTrimmerEditor);
+    }
+
+
+    @PostMapping(
+            produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE},
+            consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
+    public ResponseEntity<?> create(@Valid @RequestBody UserRegisterRequest userDetailsRequest,
+                                    BindingResult bindingResult) {
+        if (bindingResult.hasErrors())
+            return new ResponseEntity<>(new HttpResponse(HttpMessage.USER_REGISTRATION_FAILED), HttpStatus.EXPECTATION_FAILED);
+        if (userDetailsRequest.getEmail().isEmpty())
+            return new ResponseEntity<>(new HttpResponse(HttpMessage.MISSING_REQUIRED_FIELD), HttpStatus.BAD_REQUEST);
+
+        Optional<UserDTO> existing = userService.getByEmail(userDetailsRequest.getEmail());
+        if (existing.isPresent())
+            return new ResponseEntity<>(new HttpResponse(HttpMessage.RECORD_ALREADY_EXISTS), HttpStatus.FORBIDDEN);
+
+        UserDTO userDTO = new UserDTO();
+        BeanUtils.copyProperties(userDetailsRequest, userDTO);
+        userDTO.setEncryptedPassword(encoder.encode(userDetailsRequest.getPassword()));
+        UserDTO user = userService.save(userDTO);
+        UserDetailsResponse response = new UserDetailsResponse();
+        BeanUtils.copyProperties(user, response);
+        return new ResponseEntity<>(new HttpResponse(HttpMessage.USER_REGISTRATION_SUCCESSFUL, response), HttpStatus.OK);
     }
 }
